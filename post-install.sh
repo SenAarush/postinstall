9fe314c4348c1c5eb204b0911731eef0f9491666
Aarush Sen <46532093+SenAarush@users.noreply.github.com>
2022-02-11 18:00:54 +0530

    update

--- post-install.sh
+++ post-install.sh
@@ -1,9 +1,9 @@
-sudo apt autoremove kwrite vlc
+sudo apt autoremove kwrite vlc -y
 sudo pkcon refresh && update -y
 ##Add the zen kernel for better performance
 sudo add-apt-repository ppa:damentz/liquorix
 ##Add brave repo
-sudo apt install apt-transport-https curl
+sudo apt install apt-transport-https curl -y
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 ##Add vscodium repo
@@ -24,8 +24,8 @@ curl -o https://protonvpn.com/download/protonvpn-stable-release_1.0.1-1_all.deb
 sudo apt-get install /protonvpn-stable-release_1.0.1-1_all.deb
 ##Install packages
 sudo apt update
-sudo apt install muon mpv neofetch kdenlive codium slimbookbattery brave-browser linux-image-liquorix-amd64 linux-headers-liquorix-amd64 obs-studio gnome-boxes protonvpn
+sudo apt install muon mpv neofetch kdenlive codium slimbookbattery brave-browser linux-image-liquorix-amd64 linux-headers-liquorix-amd64 obs-studio gnome-boxes protonvpn -y
 sudo apt autoremove
 ## Add custom cursor theme
 git clone https://github.com/SenAarush/cursor
-sudo mv macOSMonterey /usr/share/icons 
+sudo mv cursor /usr/share/icons 
