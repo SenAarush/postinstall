440567b82a48440960258b849941b3d5c81463f9
Aarush Sen <46532093+SenAarush@users.noreply.github.com>
2022-02-11 18:06:49 +0530

    resolved some minor bugs

--- post-install.sh
+++ post-install.sh
@@ -1,7 +1,7 @@
 sudo apt autoremove kwrite vlc -y
 sudo pkcon refresh && update -y
 ##Add the zen kernel for better performance
-sudo add-apt-repository ppa:damentz/liquorix
+sudo add-apt-repository ppa:damentz/liquorix -y
 ##Add brave repo
 sudo apt install apt-transport-https curl -y
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
@@ -13,9 +13,9 @@ wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.g
 echo 'deb [ signed-by=/usr/share/keyrings/vscodium-archive-keyring.gpg ] https://download.vscodium.com/debs vscodium main' \
     | sudo tee /etc/apt/sources.list.d/vscodium.list
 ##Add slimbook battery repo
-sudo add-apt-repository ppa:slimbook/slimbook
+sudo add-apt-repository ppa:slimbook/slimbook -y
 ##Add Kdenlive repo
-sudo add-apt-repository ppa:kdenlive/kdenlive-stable
+sudo add-apt-repository ppa:kdenlive/kdenlive-stable -y
 ##Install ani-cli
 git clone https://github.com/pystardust/ani-cli && cd ani-cli
 sudo cp ani-cli /usr/local/bin/ani-cli
